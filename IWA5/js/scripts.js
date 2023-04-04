047d60c425cb3e3e1f6791b5f729babe576ef43f
TshepiMore <125869424+TshepiMore@users.noreply.github.com>
2023-04-04 15:36:37 +0200

    IWA6

--- IWA5/js/scripts.js
+++ IWA5/js/scripts.js
@@ -1,19 +1,26 @@
 const FREE_WARNING = 'Free shipping only applies to single customer orders';
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
-const NONE_SELECTED = '0';
+const NONE_SELECTED = 0;
 
-let shipping = null;
+let location = 'RSA';
 let currency = 'R';
-
-const location = 'RSA';
 let customers = 1;
+let shipping = 400;
 
-if (location === 'RSA') {
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;
+const batteries = 35 * 2;
+const pens = 5 * NONE_SELECTED ;
+
+let subtotal = shoes + toys + shirts + batteries + pens;
+
+if (location === 'RSA'){
     shipping = 400;
-    currency = 'R' }
+ }
 
 else if (location === 'NAM'){
-let shipping = 600;
+     shipping = 600;
 }
 else if (location === 'NK'){
     console.log(BANNED_WARNING);
@@ -22,19 +29,15 @@ else {
     shipping = 800;
 }
 
-const shoes = 300 * 1;
-const toys = 100 * 5;
-const shirts = 150 * 'NONE_SELECTED';
-const batteries = 35 * 2;
-const pens = 5 * 'NONE_SELECTED' ;
-
-const subtotal = shoes + toys + shirts + batteries + pens;
-
-if (subtotal >= 1000 && (location ==='RSA' || location === 'NAM') && customers === 1){
+if (subtotal > 1000 && location ==='RSA' || location === 'NAM'&& customers === 1){
 shipping = 0;
 }
-else if (shipping !== null && customers !== 1){
+else if (shipping === undefined)
+console.log(BANNED_WARNING);
+
+else if ((shipping === 0) && (customers !== 1)){
     console.log(FREE_WARNING);
 }
+const total = subtotal + shipping;
 
-console.log('Price', currency, subtotal + shipping);
+console.log('Price:', currency, total);
