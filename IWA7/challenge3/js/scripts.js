029e13e5f15da0cfd39f34a293af8dc334abd42d
TshepiMore <125869424+TshepiMore@users.noreply.github.com>
2023-04-05 16:00:41 +0200

    IWA7

--- IWA7/challenge3/js/scripts.js
+++ IWA7/challenge3/js/scripts.js
@@ -11,7 +11,7 @@ const divider = '----------------------------------'
 // Only change below this line
 
 const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = `${leoName} ${leoSurname} ("Owed : R ${parseFloat(leoBalance).toFixed(2)}\n`;
+const leo = `${leoName} ${leoSurname} (Owed : R ${parseFloat(leoBalance).toFixed(2)}\n`;
 const sarah = `${sarahName} ${sarahSurname} (Owed : R ${parseFloat(sarahBalance).toFixed(2)})\n`;
 const total = `Total amount owed: R ${(parseFloat(leoBalance) + parseFloat(sarahBalance)).toFixed(2)}\n`;
 const result = `\n${leo}${sarah}${divider}\n${total}${divider}`;
