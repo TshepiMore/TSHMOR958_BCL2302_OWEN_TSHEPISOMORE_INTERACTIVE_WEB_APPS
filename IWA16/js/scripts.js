b19d3efa90aa556c90e47760cb13f091062383fd
TshepiMore <125869424+TshepiMore@users.noreply.github.com>
2023-04-26 11:49:31 +0200

    IWA17

--- IWA16/js/scripts.js
+++ IWA16/js/scripts.js
@@ -68,61 +68,53 @@ const MONTHS = [
   };
 
   //Only edit below this comment
-  //nwabisa//
-  // Define variables for the first athlete
-let Athlete  = data.response.data.NM372.firstName +' '+ data.response.data.NM372.surname
-let races = Object.keys(data.response.data.NM372.races).length
-let day = new Date(data.response.data.NM372.races[1].date).getDate() +' '+ MONTHS[11] +' '+ new Date(data.response.data.NM372.races[1].date).getFullYear()
-console.log(day)
-let timeAsArray = data.response.data.NM372.races[1].time[0] + data.response.data.NM372.races[1].time[1] + data.response.data.NM372.races[1].time[2] + data.response.data.NM372.races[1].time[3]
-timeAsArray = '00:'+ timeAsArray
+  const createHtml = (athlete) => {//The function takes an athlete ID as an argument and creates an HTML fragment with the athlete's information
+    // Reassign the variables to make them easier to access
+    const firstName = data.response.data[athlete].firstName;
+    const surname = data.response.data[athlete].surname;
+    const id = data.response.data[athlete].id;
+    const races = (data.response.data[athlete].races).length
+    // Get the latest race date and time for the athlete
+    const date = new Date(data.response.data[athlete].races[races-1].date)
+    const time = data.response.data[athlete].races[races-1].time;
 
-// schalk//
-// Define variables for the second athlete
-const athleteSchalk = data.response.data.SV782.firstName +' '+ data.response.data.SV782.surname
-let racesSchalk = Object.keys(data.response.data.SV782.races).length
-let daySchalk = new Date(data.response.data.SV782.races.at(-1).date).getDate() +' '+ MONTHS[11] +' '+ new Date(data.response.data.NM372.races.at(-1).date).getFullYear()
-let timeAsArraySchalk = data.response.data.SV782.races[1].time[0] + data.response.data.SV782.races[1].time[1] + data.response.data.SV782.races[1].time[2] + data.response.data.SV782.races[1].time[3]
-timeAsArraySchalk = '00:'+ timeAsArraySchalk
+    // Create a document fragment to hold the HTML
+    const fragment = document.createDocumentFragment();
 
-//OUTPUT//
+    // Create a title element with the athlete's ID and append it to the fragment
+    let title = document.createElement('h2');
+    title.textContent = id
+    fragment.appendChild(title);
 
-const element = document.querySelector("body");// Select the <body> element.
-const fragment = document.createDocumentFragment('dl');// Create a new document fragment with the <dl> tag
-// Create an array of two athletes, each with their own race data
-const athlete = [
-  [ 'Athlete:' +' '+ Athlete,"Total Races: "+ races, 'Event Date : ' + day, 'Total Time: ' + timeAsArray],
-  [ 'Athlete:' +' '+ athleteSchalk,"Total Races: "+ racesSchalk, 'Event Date : ' + daySchalk, 'Total Time: ' + timeAsArraySchalk],
-]
-// Separate the two athletes into their own variables for easier reference
-let nwabisa = athlete[0]
-let schalk = athlete[1]
-// Loop through the array of athletes and their race data
-for (let i = 0; i < 3; i = i + 1){
-  if (i < 1){ // For the first athlete (nwabisa)
-    let title = document.createElement("h2");// Create a new <h2> element with the athlete's ID as text content
-    title.textContent = data.response.data.NM372.id;
-    const dl = document.querySelector('body');// Select the <body> element again
-    dl.appendChild(title); // Append the <h2> element to the <body> element
-// Loop through the athlete's race data and create new <dd> elements with the data as text content
-    nwabisa.forEach((athletes) => {
-      const details = document.createElement("dd");
-      details.textContent = athletes;
-      fragment.appendChild(details);
-       });
-       element.appendChild(fragment);// Append the entire fragment (with all <dd> elements) to the <body> element
-    } else if ( i > 1 ){
-       let titleSchalk = document.createElement("h2");// Create a new <h2> element with the athlete's ID as text content
-      titleSchalk.textContent = data.response.data.SV782.id;
-      const dd = document.querySelector('body');// Select the <body> element again
-      dd.appendChild(titleSchalk); // Append the <h2> element to the <body> element
+    // Create a list element to hold the athlete's information
+    const list = document.createElement('dl');
+     // Get the day, month, and year of the latest race date
+    const day =  date.getDate()
+    const month = MONTHS[date.getMonth()];
+    const year = date.getFullYear();
+    console.log(month)
 
-      schalk.forEach((athletes) => {
-      const details = document.createElement("dd");// Loop through the athlete's race data and create new <dd> elements with the data as text content
-      schalk.forEach((athletes) => {
-      details.textContent = athletes;
-      fragment.appendChild(details);
-       });
-       element.appendChild(fragment);// Append the entire fragment (with all <dd> elements) to the <body> element
+    // Calculate the total time for the latest race
+    let sum = 0
+    let i = 0;
+    while ( i < time.length ) {
+      sum += time[i]
+      i++
     }
-}
+    const minutes = sum % 60;
+    const hours = (sum - minutes) / 60;
+
+    // Fill the list with the athlete's information
+    list.innerHTML = /* html */ `
+        <dt>Athlete: ${firstName +' '+ surname}</dt>
+        <dt>Total Races: ${races}</dt>
+        <dt>Event Date (Latest): ${day.toString().padStart(2, '0')+' '+ month +' '+ year}</dt>
+        <dt>Total Time (Latest): ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}</dt>
+      `;
+      // Append the list to the fragment
+    fragment.appendChild(list);
+    return fragment;
+  }
+ // Get the elements with data-athlete attributes and append the HTML fragment with the athlete's information to each element
+  document.querySelector('[data-athlete = "NM372"]').appendChild(createHtml('NM372'));
+  document.querySelector('[data-athlete ="SV782"]').appendChild(createHtml('SV782'));
